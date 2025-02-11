aa336e8604c4e822077d0b738f8d807203428b6b
murari <zexe063@gmail.com>
2025-02-11 20:38:37 +0530

    fix ReportQuestion

--- backend/route/ReportQuestionRoute.js
+++ backend/route/ReportQuestionRoute.js
@@ -4,8 +4,8 @@ const express = require("express");
 const ReportQuestionRouter = express.Router();
 const {getReportQuestion , createReportQuestion} = require("../controller/ReportQuestionController");
 
-ReportQuestionRouter.get("/ReportQuestion", getReportQuestion)
-ReportQuestionRouter.post("/ReportQuestion", createReportQuestion);
+ReportQuestionRouter.get("/", getReportQuestion)
+ReportQuestionRouter.post("/", createReportQuestion);
 
  module.exports = ReportQuestionRouter;
 
